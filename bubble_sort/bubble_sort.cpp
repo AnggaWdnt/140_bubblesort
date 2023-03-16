535d9d1e5af1936677bc423aef6cca4b0f126ab6
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-03-16 16:05:42 +0700

    tambahan void input

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -2,10 +2,27 @@
 //
 
 #include <iostream>
+using namespace std;
 
+int a[20];							// Deklarasi array a dengan ukuran 20
+int n;								// Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+void input() {						// procedur unutk input
+	while (true) {				// looping
+		cout << "Masukan banyaknya elemen pada array : ";	// Output ke layar
+		cin >> n;				// Input dari pengguna
+		if (n <= 20)			// Jika n kurang dari atau sama dengan 20
+			break;				// Keluar dari loop
+		else (					// Jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Output ke layar
+	}
+}
+cout << endl;								// Output baris kosong
+cout << "===================" << edl;		// Output ke layar
+cout << "Masukan Elemen Array" << endl;		// Output ke layar
+cout << "===================" << endl;		// Output ke layar
 int main()
 {
-    std::cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
